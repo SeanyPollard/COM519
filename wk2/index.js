ab7cc103045280eae5edd0cb36db2a3bc56b5652
Sean Pollard <seanypollard@wstudent-79-161-35.solent.ac.uk>
2021-10-06 15:37:41 +0100

    week 2 done

---
+++ wk2/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index");
+  });
+
+app.get("/about", (req, res) => {
+    res.render("about");
+  });
+
+app.get("/contact", (req, res) => {
+    res.render("contact");
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
